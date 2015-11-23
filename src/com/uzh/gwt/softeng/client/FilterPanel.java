c9d58af7a25e7b21fe5bf2c6ea265f3a96bc6400
antoio <antonio.galluccio@gmail.com>
2015-11-23 16:08:23 +0100

    adjusted Heatmap and filterTable Div

--- src/com/uzh/gwt/softeng/client/FilterPanel.java
+++ src/com/uzh/gwt/softeng/client/FilterPanel.java
@@ -3,7 +3,8 @@ package com.uzh.gwt.softeng.client;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.SimplePanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
@@ -12,21 +13,28 @@ public class FilterPanel extends Composite {
 	private VerticalPanel vlp;
 
 	//dummy content
-	private SimplePanel panel1;
-	private SimplePanel panel2;
+	private HorizontalPanel panel1;
+	private HorizontalPanel panel2;
 
 	private TextBox textBox = new TextBox();
 	private CheckBox checkBox = new CheckBox();
+	private Label textBoxLabel = new Label("test input");
+	private Label checkBoxLabel = new Label("test checkBox");
 
 	public FilterPanel() {
 		//initialization
 		vlp = new VerticalPanel();
-		panel1 = new SimplePanel();
-		panel2 = new SimplePanel();
+		panel1 = new HorizontalPanel();
+		panel2 = new HorizontalPanel();
 
 		//add components to vertical panel
+		panel1.add(textBoxLabel);
 		panel1.add(textBox);
+		panel1.setHeight("100px");
+		
+		panel2.add(checkBoxLabel);
 		panel2.add(checkBox);
+		panel2.setHeight("100px");
 
 		vlp.add(panel1);
 		vlp.add(panel2);
